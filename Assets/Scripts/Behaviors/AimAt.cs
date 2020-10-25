0ac9a6c3efa60e66b50d6156b0c6aaf23c0b17fa
cryscan <cryscan@umich.edu>
2020-10-25 04:49:08 -0400

    Add more guards.

--- Assets/Scripts/Behaviors/AimAt.cs
+++ Assets/Scripts/Behaviors/AimAt.cs
@@ -6,7 +6,7 @@ public class AimAt : Action
 {
     public SharedGameObject subject;
     public SharedVector3 target;
-    public float fallout = 20;
+    public SharedFloat fallout = 20;
 
     public override void OnStart()
     {
@@ -19,7 +19,7 @@ public class AimAt : Action
         direction.y = 0;
 
         var angle = Vector3.SignedAngle(transform.forward, direction, Vector3.up);
-        angle = 0f.Fallout(angle, fallout);
+        angle = 0f.Fallout(angle, fallout.Value);
         transform.Rotate(0, angle, 0, Space.Self);
 
         if (Mathf.Abs(angle) < 0.1) return TaskStatus.Success;
