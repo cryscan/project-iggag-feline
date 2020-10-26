dc315d45564a44dc03408f4bc2f10f8360e2dbe2
edwardwhe <he_yu_jia@sjtu.edu.cn>
2020-10-25 22:03:53 -0400

    Trap Handler and Guard Trap Reaction

---
+++ Assets/Scripts/Enemy/GuardTrapReaction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardTrapReaction : MonoBehaviour
+{
+    Subscription<TrapEvent> trapEventHandler;
+
+    private void OnEnable()
+    {
+        trapEventHandler = EventBus.Subscribe<TrapEvent>(OnTrap);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe(trapEventHandler);
+    }
+
+    void OnTrap(TrapEvent @event)
+    {
+        if (@event.target == this.gameObject)
+        {
+            switch(@event.type)
+            {
+                case TrapType.Frozen:
+                    break;
+                case TrapType.Eliminate:
+                    break;
+            }
+        }
+    }
+
+    void Frozen()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Trap/TrapHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TrapType
+{
+    Frozen,
+    Eliminate
+}
+
+public class TrapEvent 
+{
+    public GameObject target;
+    public TrapType type;
+
+    public TrapEvent(GameObject target, TrapType type)
+    {
+        this.target = target;
+        this.type = type;
+    }
+}
+
+public class TrapHandler : MonoBehaviour
+{
+    [SerializeField] TrapType trapType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject _object = other.gameObject;
+        if (_object.CompareTag("Enemy"))
+        {
+            EventBus.Publish(new TrapEvent(_object, trapType));
+        }        
+    }
+}
