91494d60cc845df9e25abe8f1b06869581492fc3
cryscan <cryscan@umich.edu>
2020-10-25 20:14:56 -0400

    Camera reaction.

---
+++ Assets/Scripts/Enemy/CameraReaction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraReaction : MonoBehaviour
+{
+    [SerializeField] Light _light;
+    [SerializeField] Color dealertedLightColor = Color.white;
+    [SerializeField] Color alertedLightColor = Color.red;
+    [SerializeField] float alertedTime = 1;
+
+    bool alerted = false;
+    float currentAltertedTime = 0;
+
+    Subscription<DetectEvent> detectEventHandler;
+
+    void OnEnable()
+    {
+        detectEventHandler = EventBus.Subscribe<DetectEvent>(OnDetected);
+    }
+
+    void OnDisable()
+    {
+        EventBus.Unsubscribe(detectEventHandler);
+    }
+
+    void Update()
+    {
+        if (currentAltertedTime > 0)
+            currentAltertedTime -= Time.deltaTime;
+        else if (alerted)
+        {
+            alerted = false;
+            _light.color = dealertedLightColor;
+        }
+    }
+
+    void OnDetected(DetectEvent @event)
+    {
+        if (@event.subject != gameObject) return;
+
+        alerted = true;
+        _light.color = alertedLightColor;
+        currentAltertedTime = alertedTime;
+    }
+}
