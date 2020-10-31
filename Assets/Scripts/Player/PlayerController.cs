b403517a45b071057e717fad2c7594fc7904b9a1
cryscan <cryscan@umich.edu>
2020-10-31 02:36:56 -0400

    Test emmisive lighting.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    Stand,
+    Move,
+    Sprint,
+}
+
+public enum PlayerIdentity
+{
+    Guest,
+    Worker,
+    Guard,
+}
+
+public class PlayerStateChangeEvent
+{
+    public PlayerState current, previous;
+
+    public PlayerStateChangeEvent(PlayerState current, PlayerState previous)
+    {
+        this.current = current;
+        this.previous = previous;
+    }
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] PlayerState _state;
+    public PlayerState state { get => _state; }
+
+    [SerializeField] PlayerIdentity _identity;
+    public PlayerIdentity identity { get => _identity; }
+
+    
+}
