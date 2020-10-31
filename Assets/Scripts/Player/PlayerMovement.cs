85a067ae7510a18473e7737bdf1e3b6c7fbcc548
cryscan <cryscan@umich.edu>
2020-10-31 04:51:06 -0400

    Player sprint.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,37 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    Stand,
+    Move,
+    Sprint,
+}
+
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] bool global = true;
-    [SerializeField] float speed = 4;
+    [SerializeField] float normalSpeed = 4;
+    [SerializeField] float sprintSpeed = 8;
     [SerializeField] float fallout = 10;
 
+    [SerializeField] PlayerState _state;
+    public PlayerState state { get => _state; }
+
     CharacterController controller;
+    float speed;
     Vector3 _move;
 
     void Awake()
     {
         controller = GetComponent<CharacterController>();
+        speed = normalSpeed;
     }
 
     void Update()
     {
-        Move();
-    }
-
-    void Move()
-    {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
-        Vector3 move;
-        if (global) move = Vector3.forward * vertical + Vector3.right * horizontal;
-        else move = transform.forward * vertical + transform.right * horizontal;
+        if (Input.GetKey(KeyCode.LeftShift)) speed = sprintSpeed;
+        else speed = normalSpeed;
 
+        var move = transform.forward * vertical + transform.right * horizontal;
         move = move.normalized;
 
         _move = Vector3.Lerp(_move, move, 1 - Mathf.Exp(-fallout * Time.deltaTime));
-        controller.SimpleMove(_move * speed);
+        var velocity = _move * speed;
+        controller.SimpleMove(velocity);
+
+        var realSpeed = velocity.magnitude;
+        if (realSpeed < normalSpeed / 2) _state = PlayerState.Stand;
+        else if (realSpeed < (normalSpeed + sprintSpeed) / 2) _state = PlayerState.Move;
+        else _state = PlayerState.Sprint;
     }
 }
