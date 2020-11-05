5b053d7b2b72d1ec291f356b0de1cf7f830192db
evansdav <evansdav@umich.edu>
2020-11-05 17:32:24 -0500

    Dialogue system is working

---
+++ Assets/Scripts/Menu/DialogueManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+	public Text dialogueText;
+
+	public Queue<string> sentences;
+
+	public GameObject canvas;
+
+	public GameObject dialogueParent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+    	//Pause Game here
+    	GameManager.instance.TogglePause();
+    	canvas.GetComponent<GraphicRaycaster>().enabled = true;
+
+    	sentences.Clear();
+    	foreach (string sentence in dialogue.sentences) {
+    		sentences.Enqueue(sentence);
+    	}
+    	DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+    	if (sentences.Count == 0) {
+    		EndDialogue();
+    		return;
+    	}
+    	string sentence = sentences.Dequeue();
+    	StopAllCoroutines();
+    	StartCoroutine(TypeSentence(sentence));
+    	Debug.Log("SENTENCE DISPLAYED");
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+    	dialogueText.text = "";
+    	foreach (char letter in sentence.ToCharArray()) {
+    		dialogueText.text += letter;
+    		yield return WaitForRealSeconds(0.025f);
+    	}
+    }
+
+    IEnumerator WaitForRealSeconds(float time)
+    {
+        float start = Time.realtimeSinceStartup;
+        while (Time.realtimeSinceStartup < start + time)
+        {
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+    	Debug.Log("End of conversation");
+    	//Unpause Game here
+    	GameManager.instance.TogglePause();
+    	canvas.GetComponent<GraphicRaycaster>().enabled = false;
+    	dialogueParent.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Menu/DialogueTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DialogueTrigger : MonoBehaviour
+{
+	public Dialogue dialogue;
+	public TextAsset file;
+	Subscription<GameStateChangeEvent> gameStateChangeHandler;
+	public GameObject dialogueParent;
+	
+    // Start is called before the first frame update
+    void Awake()
+    {
+    	string[] input = file.text.Split('\n');
+    	dialogue = new Dialogue();
+    	dialogue.sentences = input;
+    	gameStateChangeHandler = EventBus.Subscribe<GameStateChangeEvent>(OnGameStateChanged);
+    	dialogueParent.SetActive(false); 
+    }
+
+    public void TriggerDialogue ()
+    {
+    	FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    void OnGameStateChanged(GameStateChangeEvent @event)
+    {
+        if (@event.previous == GameState.Start && @event.current == GameState.Plan)
+        {
+        	dialogueParent.SetActive(true);
+        	TriggerDialogue();
+        }
+    }
+}
