4dd9d649896e5708bf76f0428040e1583423a9f0
cryscan <cryscan@umich.edu>
2020-11-08 19:13:19 -0500

    Import icons.

--- Assets/Scripts/LinearProgressBar.cs
+++ Assets/Scripts/LinearProgressBar.cs
@@ -8,6 +8,18 @@ public class LinearProgressBar : MonoBehaviour
     [SerializeField] Image mask;
     public float max;
     public float current;
+    public float amount { get => current / max; }
+
+    public Vector3 startPoint { get; private set; }
+    public Vector3 endPoint { get; private set; }
+
+    void Awake()
+    {
+        var position = mask.transform.position;
+        var width = mask.rectTransform.rect.width;
+        startPoint = new Vector3(position.x - width, position.y, position.z);
+        endPoint = new Vector3(position.x + width, position.y, position.z);
+    }
 
     void Update()
     {
---
+++ Assets/Scripts/Traps/FrozenTrap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrozenTrap : TrapBase
+{
+    [SerializeField] float _range = 5;
+    public float range { get => _range; }
+
+    [SerializeField] float _duration = 5;
+    public float duration { get => _duration; }
+}
