061f549b74d0e42097fa2760e666c9395f6e06fa
edwardwhe <he_yu_jia@sjtu.edu.cn>
2020-11-09 07:30:09 -0500

    add UI for counter

---
+++ Assets/Scripts/CounterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterController : MonoBehaviour
+{
+    [SerializeField] Text frozenCount;
+    [SerializeField] Text distractionCount;
+
+    public void SetFrozenCount(float num)
+    {
+        frozenCount.text = num.ToString();
+    }
+
+    public void SetDistractionCount(float num)
+    {
+        distractionCount.text = num.ToString();
+    }
+}
--- Assets/Scripts/Traps/PlaceTrap.cs
+++ Assets/Scripts/Traps/PlaceTrap.cs
@@ -11,15 +11,18 @@ public class PlaceTrap : MonoBehaviour
     [SerializeField] LayerMask blockLayers;
     [SerializeField] float count;
     [SerializeField] Sprite sprite;
+    [SerializeField] string trapType;
 
     [SerializeField] GameObject ball;
     Camera _camera;
+    CounterController Counter;
 
     private void Awake()
     {
         _camera = Camera.main;
         ball.transform.localScale = new Vector3(radius, radius, radius);
         Cursor.visible = false;
+        Counter = GameObject.FindGameObjectWithTag("Counter").GetComponent<CounterController>();
     }
 
     private void Update()
@@ -39,6 +42,11 @@ public class PlaceTrap : MonoBehaviour
             //    Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
             //    Cursor.visible = true;
             }
+
+            if (trapType == "frozen")
+                Counter.SetFrozenCount(count);
+            else
+                Counter.SetDistractionCount(count);
 
             if (valid && Input.GetMouseButtonDown(0))
                 InstantiateTrap();
