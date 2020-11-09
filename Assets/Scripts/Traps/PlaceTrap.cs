7c97bc4ded131238cc37b5b51aa47418c18af3dd
edwardwhe <he_yu_jia@sjtu.edu.cn>
2020-11-09 08:31:35 -0500

    modify text for basic tutorial

--- Assets/Scripts/Traps/PlaceTrap.cs
+++ Assets/Scripts/Traps/PlaceTrap.cs
@@ -87,7 +87,7 @@ public class PlaceTrap : MonoBehaviour
         
 
         Vector3 position = new Vector3(Mathf.Round(transform.position.x),
-                                             0f,
+                                             Mathf.Round(transform.position.y),
                                              Mathf.Round(transform.position.z));
 
 
