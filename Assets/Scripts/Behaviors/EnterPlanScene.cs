2471bc994b13c7dc132fb8372e5f996f25a1512d
cryscan <cryscan@umich.edu>
2020-11-16 10:41:13 -0500

    Fix unlock.

--- Assets/Scripts/Behaviors/EnterPlanScene.cs
+++ Assets/Scripts/Behaviors/EnterPlanScene.cs
@@ -5,11 +5,9 @@ using BehaviorDesigner.Runtime.Tasks;
 public class EnterPlanScene : Action
 {
     public string name;
-    public int index;
 
     public override TaskStatus OnUpdate()
     {
-        MainMenuController.UnlockLevel(index);
         GameManager.instance.EnterPlanSceneRelocate(name);
         return TaskStatus.Success;
     }
--- Assets/Scripts/Behaviors/EnterPlayScene.cs
+++ Assets/Scripts/Behaviors/EnterPlayScene.cs
@@ -5,11 +5,9 @@ using BehaviorDesigner.Runtime.Tasks;
 public class EnterPlayScene : Action
 {
     public string name;
-    public int index;
 
     public override TaskStatus OnUpdate()
     {
-        MainMenuController.UnlockLevel(index);
         GameManager.instance.EnterPlaySceneRelocate(name);
         return TaskStatus.Success;
     }
