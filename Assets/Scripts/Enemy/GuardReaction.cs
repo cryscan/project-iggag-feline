4855bdf1fa44ccde6b008a01faca42f677fc54ed
cryscan <cryscan@umich.edu>
2020-11-16 08:44:55 -0500

    Amend distraction.

--- Assets/Scripts/Enemy/GuardReaction.cs
+++ Assets/Scripts/Enemy/GuardReaction.cs
@@ -178,7 +178,8 @@ public class GuardReaction : MonoBehaviour
         else if (@event.trap is DistractionTrap)
         {
             DistractionTrap distraction = (DistractionTrap)@event.trap;
-            if (distance < distraction.range && !distraction.ReachedMaxCount() && !frozen)
+            // if (distance < distraction.range && !distraction.ReachedMaxCount() && !frozen && !searching)
+            if (!distraction.ReachedMaxCount() && !frozen && !searching)
             {
                 behavior.SetVariableValue("Alerted", true);
                 behavior.SetVariableValue("Spot Point", position);
