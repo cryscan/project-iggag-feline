89278815094b6734f61dc6ba0a524291e12a103e
cryscan <cryscan@umich.edu>
2020-11-29 15:09:36 -0500

    Basic heist board.

---
+++ Assets/Scripts/Interaction/HeistBoard.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeistBoard : MonoBehaviour
+{
+    [SerializeField] bool _practice = false;
+    public bool practice { get => _practice; }
+}
