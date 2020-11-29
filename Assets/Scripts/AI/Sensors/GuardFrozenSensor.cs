16f8bfbcb9f4164e0ef09f3a5d5e926bcff55a6d
cryscan <cryscan@umich.edu>
2020-11-28 23:02:32 -0500

    Fix guard alert sensor and frozen sensor.

--- Assets/Scripts/AI/Sensors/GuardFrozenSensor.cs
+++ Assets/Scripts/AI/Sensors/GuardFrozenSensor.cs
@@ -7,86 +7,96 @@ using BehaviorDesigner.Runtime;
 
 using ReGoap.Unity;
 
-[RequireComponent(typeof(BehaviorTree))]
-[RequireComponent(typeof(NavMeshAgent))]
-public class GuardFrozenSensor : ReGoapSensor<string, object>
+namespace Feline.AI.Sensors
 {
-    [SerializeField] GameObject effect;
-    [SerializeField] Light _light;
-
-    bool frozen;
-
-    Collider _collider;
-    ConeDetection detection;
-    BehaviorTree behavior;
-    NavMeshAgent agent;
-
-    Subscription<TrapActivateEvent> trapActivateHandler;
-
-    void Awake()
+    [RequireComponent(typeof(BehaviorTree))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class GuardFrozenSensor : ReGoapSensor<string, object>
     {
-        _collider = GetComponent<Collider>();
-        detection = GetComponent<ConeDetection>();
-        behavior = GetComponent<BehaviorTree>();
-        agent = GetComponent<NavMeshAgent>();
-    }
+        [SerializeField] GameObject effect;
+        [SerializeField] Light _light;
 
-    void OnEnable()
-    {
-        EventBus.Subscribe<TrapActivateEvent>(OnTrapActivated);
-    }
+        public bool frozen { get; private set; }
 
-    void OnDisable()
-    {
-        EventBus.Unsubscribe(trapActivateHandler);
-    }
+        Collider _collider;
+        ConeDetection detection;
+        GuardAlertSensor sensor;
+        BehaviorTree behavior;
+        NavMeshAgent agent;
 
-    public override void UpdateSensor()
-    {
-        effect.SetActive(frozen);
-    }
+        bool stopped = false;
 
-    void OnTrapActivated(TrapActivateEvent @event)
-    {
-        var position = @event.trap.transform.position;
+        Subscription<TrapActivateEvent> trapActivateHandler;
 
-        if (@event.trap is FrozenTrap)
+        void Awake()
         {
-            var frozen = @event.trap as FrozenTrap;
-            var distance = Vector3.Distance(transform.position, frozen.transform.position);
+            _collider = GetComponent<Collider>();
+            detection = GetComponent<ConeDetection>();
+            sensor = GetComponent<GuardAlertSensor>();
+            behavior = GetComponent<BehaviorTree>();
+            agent = GetComponent<NavMeshAgent>();
+        }
 
-            if (distance < frozen.range) Freeze(frozen.duration);
+        void OnEnable()
+        {
+            EventBus.Subscribe<TrapActivateEvent>(OnTrapActivated);
+        }
+
+        void OnDisable()
+        {
+            EventBus.Unsubscribe(trapActivateHandler);
+        }
+
+        public override void UpdateSensor()
+        {
+            effect.SetActive(frozen);
+        }
+
+        void OnTrapActivated(TrapActivateEvent @event)
+        {
+            var position = @event.trap.transform.position;
+
+            if (@event.trap is FrozenTrap)
+            {
+                var frozen = @event.trap as FrozenTrap;
+                var distance = Vector3.Distance(transform.position, frozen.transform.position);
+
+                if (distance < frozen.range) Freeze(frozen.duration);
+            }
+        }
+
+        void Freeze(float duration)
+        {
+            StopAllCoroutines();
+            StartCoroutine(FrozenCoroutine(duration));
+        }
+
+        IEnumerator FrozenCoroutine(float duration)
+        {
+            _light.enabled = false;
+            _collider.enabled = false;
+            detection.enabled = false;
+            sensor.enabled = false;
+
+            behavior.DisableBehavior(true);
+
+            if (!frozen) stopped = agent.isStopped;
+            agent.isStopped = true;
+
+            frozen = true;
+
+            yield return new WaitForSeconds(duration);
+
+            frozen = false;
+
+            _light.enabled = true;
+            _collider.enabled = true;
+            detection.enabled = true;
+            sensor.enabled = true;
+
+            behavior.EnableBehavior();
+            agent.isStopped = stopped;
         }
     }
 
-    void Freeze(float duration)
-    {
-        StopAllCoroutines();
-        StartCoroutine(FrozenCoroutine(duration));
-    }
-
-    IEnumerator FrozenCoroutine(float duration)
-    {
-        frozen = true;
-
-        _light.enabled = false;
-        _collider.enabled = false;
-        detection.enabled = false;
-
-        behavior.DisableBehavior(true);
-
-        var stopped = agent.isStopped;
-        agent.isStopped = true;
-
-        yield return new WaitForSeconds(duration);
-
-        frozen = false;
-
-        _light.enabled = true;
-        _collider.enabled = true;
-        detection.enabled = true;
-
-        behavior.EnableBehavior();
-        agent.isStopped = stopped;
-    }
 }
