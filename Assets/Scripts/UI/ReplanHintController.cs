5a90bfcc23519b900e3497987ed69097e910721e
cryscan <cryscan@umich.edu>
2020-12-05 01:57:55 -0500

    Not showing plan hint in heist 0.

---
+++ Assets/Scripts/UI/ReplanHintController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplanHintController : MonoBehaviour
+{
+    void Start()
+    {
+        if (GameManager.instance.startState == GameState.Play)
+            gameObject.SetActive(false);
+    }
+}
